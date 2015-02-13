de90b7d9a7d6725e80e66f58f077b7a17376bccc
dcuddhy <dcuddhy@gmail.com>
2015-02-13 14:32:07 -0700

    retweeked google fonts exceptions. should work but with errors.

--- config/environment.js
+++ config/environment.js
@@ -17,10 +17,10 @@ module.exports = function(environment) {
 contentSecurityPolicy: {
       'default-src': "'none'",
       'script-src': "'self' 'unsafe-eval'",
-      'font-src': "'self' //fonts.gstatic.com",
+      'font-src': "'self' fonts.gstatic.com",
       'connect-src': "'self' " + process.env.ADAPTER_URL,
       'img-src': "'self'",
-      'style-src': "'self' 'unsafe-inline' //fonts.googleapis.com //maxcdn.bootstrapcdn.com",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com maxcdn.bootstrapcdn.com",
       'media-src': "'self'"
     },
 
